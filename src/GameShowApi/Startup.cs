cbc781e24ccbaacd836f27d5d04ef033d0d2f3d1
linton-mack <linton.mackereth@onthebeach.co.uk>
2019-10-21 15:45:01 +0100

    basic dto for presenters set up

--- src/GameShowApi/Startup.cs
+++ src/GameShowApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameShowApi
 {
@@ -16,25 +17,28 @@ namespace GameShowApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc((MvcOptions) =>
+            {
+                MvcOptions.EnableEndpointRouting = false;
+            }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseMvc();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
+            app.Run(async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
-            });
         }
     }
 }
